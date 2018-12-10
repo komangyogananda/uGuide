43ee954ee2fc67c821fdd3e31e304ca0769a81ef
komangyogananda <yoganandamahaputra@gmail.com>
2018-12-10 16:33:31 +0700

    test conflict

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -21,7 +21,7 @@
         }
 
         public function loginAction(){
-            $tipe = $this->request->getPost('tipe');
+            $tipe = $this->request->getPost('type');
             $password = $this->request->getPost('password');
             $email = $this->request->getPost('email');
             $user = User::findFirst("email = '$email'");
